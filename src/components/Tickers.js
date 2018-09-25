a2a86479d931451ee5e01317282addffe85de68f
Richard Paredes <chard.paredes@gmail.com>
2018-09-25 15:39:46 +0200

    Added children components

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -1,40 +1,53 @@
-import React, { Component } from 'react';
-import './Tickers.css';
+import React, { Component } from "react";
+import "./Tickers.css";
+import CryptoCurrency from "./CryptoCurrency";
 
-  class Tickers extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        data: [
-          {
-            id: "bitcoin",
-            price: "1"
-          },
-          {
-            id: "ethereum",
-            price: "1"
-          },
-          {
-            id: "litecoin",
-            price: "1"
-          }
-        ]
-      }
-    }
-
-    render() {
-      const tickers = this.state.data.map((currency) => 
-        <li key={currency.id}>
-          <h3>{currency.id}</h3>
-          <h4>{currency.price} USD</h4>
-        </li>
-      );
-      return (
-        <div className="tickers-container">
-          <ul className="tickers">{tickers}</ul>
-        </div>
-      );
-    }
+class Tickers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: "bitcoin",
+          name: "Bitcoin",
+          symbol: "BTC",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0"
+        },
+        {
+          id: "ethereum",
+          name: "Ethereum",
+          symbol: "ETH",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0"
+        },
+        {
+          id: "litecoin",
+          name: "Litecoin",
+          symbol: "LTC",
+          price_usd: "1",
+          percent_change_1h: "0",
+          percent_change_24h: "0",
+          percent_change_7d: "0"
+        }
+      ]
+    };
   }
+
+  render() {
+    const tickers = this.state.data.map(currency => (
+      <CryptoCurrency data={currency} key={currency.id} />
+    ));
+    return (
+      <div className="tickers-container">
+        <ul className="tickers">{tickers}</ul>
+      </div>
+    );
+  }
+}
 
 export default Tickers;
