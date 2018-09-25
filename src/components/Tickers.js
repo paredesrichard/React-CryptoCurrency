21fb51cb5d47b1a318cedf6300a564936af41647
Richard Paredes <chard.paredes@gmail.com>
2018-09-25 15:05:57 +0200

    added state on Tickers. provided placeholder data

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import './Tickers.css';
 
   class Tickers extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [
+          {
+            id: "bitcoin",
+            price: "1"
+          },
+          {
+            id: "ethereum",
+            price: "1"
+          },
+          {
+            id: "litecoin",
+            price: "1"
+          }
+        ]
+      }
+    }
+
     render() {
-      return <p>Tickers</p>
+      const tickers = this.state.data.map((currency) => 
+        <li key={currency.id}>
+          <h3>{currency.id}</h3>
+          <h4>{currency.price} USD</h4>
+        </li>
+      );
+      return (
+        <div className="tickers-container">
+          <ul className="tickers">{tickers}</ul>
+        </div>
+      );
     }
   }
 
